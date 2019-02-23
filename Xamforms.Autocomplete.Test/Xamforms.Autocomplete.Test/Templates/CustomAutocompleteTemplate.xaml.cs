faca57d35c368ddc48e5171b28c40d57f4c1bcae
alex <alexis.enriquezc@ug.edu.ec>
2019-02-22 20:21:38 -0500

    Creating custom data template for display items of autocomplete

---
+++ Xamforms.Autocomplete.Test/Xamforms.Autocomplete.Test/Templates/CustomAutocompleteTemplate.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamforms.Autocomplete.Test.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CustomAutocompleteTemplate: ViewCell
+	{
+		public CustomAutocompleteTemplate ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
